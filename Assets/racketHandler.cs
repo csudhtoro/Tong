723f1a540bcf934f4c7b860e78dc114fce6f3ca7
Dana Shorts <32418263+csudhtoro@users.noreply.github.com>
2020-01-08 16:27:57 -0800

    Initial project commit

---
+++ Assets/racketHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class racketHandler : MonoBehaviour
+{
+
+    public float yBound = 3.0f;
+    public float speed = 10.0f;
+    public string axis = "Vertical";
+
+    // Start is called before the first frame update
+   
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        float v = Input.GetAxisRaw(axis);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v) * speed;   
+
+        var pos = transform.position;
+        if (pos.y > yBound) pos.y = yBound;
+        else if (pos.y < -yBound) pos.y = -yBound;
+
+        transform.position = pos;
+
+    }
+
+}
